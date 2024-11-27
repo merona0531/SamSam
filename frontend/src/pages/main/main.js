417fa18f3bd1c6c6c3372a508b2364ada7cc41db
user <ynzxc123@gmail.com>
2024-11-27 21:33:42 +0900

    main page 추가

--- frontend/src/pages/main/main.js
+++ frontend/src/pages/main/main.js
@@ -1,6 +1,41 @@
-export default function TrendPage() {
+import {Reset} from "styled-reset";
+import {Wrapper, MainImg , LogoImg, Container, RoundedRectangle, ButtonContainer, ButtonWrapper, Button, Title} from "./mainstyle.js";
+import {useNavigate} from "react-router-dom";
+
+
+import MainImage from '../../images/image.png'
+import Logo from '../../images/samsam.png'
+import CloudSVG from './CloudSVG';
+
+
+
+
+export default function MainPage() {
+    const navigate = useNavigate();
+
     return(
         <>
+            <Reset/>
+            <Wrapper>
+                <MainImg src={MainImage} />
+                <LogoImg src={Logo}  />
+                <CloudSVG />
+                <Container>
+                <RoundedRectangle >
+                <ButtonContainer>
+                    <Title>원하는 서비스를 선택해주세요.</Title>
+                    <ButtonWrapper>
+                        <Button>전국 체육시설 스포츠강좌 리스트</Button>
+                        <Button onClick={() => navigate('/trend')}>스포츠이용권 사용 트렌드</Button>
+                        <Button onClick={() => navigate('/recomendation/input')}>유사도 추천</Button>
+                    </ButtonWrapper>
+                </ButtonContainer>
+                </RoundedRectangle>
+                </Container>
+
+
+            </Wrapper>
         </>
     )
 }
+
---
+++ frontend/src/pages/main/mainstyle.js
@@ -0,0 +1,100 @@
+import styled from 'styled-components';
+
+
+export const Wrapper=styled.div`
+    width: 100%;
+    min-height: 100vh;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    overflow: hidden;
+    margin: 0;
+    position: relative;
+`
+
+export const MainImg = styled.img`
+    height: 100%;  
+    width: auto;  
+    object-fit: cover; 
+    position: absolute;
+    left: -100px;
+
+`;
+
+
+export const LogoImg = styled.img`
+  position: absolute;
+  top: 20px;  
+  left: 50%;
+  transform: translateX(-50%); 
+  width: 550px;
+    z-index: 2;,
+`;
+
+export const RoundedRectangle = styled.div`
+    
+    width: 45vw;
+    height: 70vh;
+    background-color: #FDB9DF;
+    position: absolute;
+    bottom: 0px;  
+    right: 0px;
+    border-top-left-radius: 60%;
+
+    min-width: 500px;
+`;
+
+export const Container = styled.div`
+    
+    width: 50vw;
+    height: 70vh;
+    position: absolute;
+    bottom: 0px;  
+    right: 0px;   
+`;
+
+
+export const ButtonContainer = styled.div`
+    width: 100%;
+    height: 80%;
+
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center; 
+    gap: 40px; 
+`;
+
+export const ButtonWrapper = styled.div`
+    width: 411px;  /* 버튼의 너비 */
+    height: 100vh; /* 버튼의 높이 (원하는 크기로 조정) */
+    display: flex;  
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 30px
+`;
+
+export const Button = styled.button`
+  width: 411px;
+  height: 70px; 
+  font-size: 22px;
+  color: #000000;
+  background-color: #FFFFFF;
+  border: none;
+  border-radius: 10px;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+
+  &:hover {
+    background-color: #FF85C1; /* 호버 효과 */
+    transform: scale(1.05); /* 약간 확대 */
+  }
+`;
+
+export const Title=styled.div`
+  font-size: 24px;
+  margin-top: 350px;
+    z-index: 2;
+`
